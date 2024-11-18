2192f762d2c3f04ec4443bbeac616d8674eb5981
Theodore Bascaron <theodore.bascaron@polymtl.ca>
2024-11-18 18:06:14 +0000

    Update 3 files
    - /sources/riscv_decode
    - /sources/riscv_execute
    - /sources/riscv_core.vhd

--- sources/riscv_core.vhd
+++ sources/riscv_core.vhd
@@ -33,47 +33,17 @@ end entity riscv_core;
 -- 5 Write-Back (WB)
 
 architecture beh of riscv_core is
--- Component declaration for all riscv_modules are in riscv_pkg
-
--- Signals Instruction Fetch
-    -- (IF) Signals for PC
-    signal pc        : std_logic_vector(XLEN-1 downto 0);             -- Program counter output 
-    signal stall     : std_logic;                                     -- Stall signal
-    signal transfert : std_logic;                                     -- Transfer signal
-    signal target    : std_logic_vector(XLEN-1 downto 0);             -- Target address for jump
-
-    -- (IF) Signals for IMEM
-    signal imem_en   : std_logic;       
-
-    -- (IF/ID) Signals for Pipeline Register
-    signal instr :  std_logic_vector(31 downto 0);
-
--- Signals Instruction Decode
-    -- (ID) Signals Register File 
-    signal we      : std_logic;                         -- Write enable for the register file
-    signal addr_r1 : std_logic_vector(REG_WIDTH-1 downto 0);  -- Read address A
-    signal data_r1 : std_logic_vector(XLEN-1 downto 0);  -- Read data A
-    signal addr_r2 : std_logic_vector(REG_WIDTH-1 downto 0);  -- Read address B
-    signal data_r2 : std_logic_vector(XLEN-1 downto 0);  -- Read data B
-    signal addr_w  : std_logic_vector(REG_WIDTH-1 downto 0);  -- Write address
-    signal data_w  : std_logic_vector(XLEN-1 downto 0);  -- Write data
-    -- (ID) Signals predecode
-    signal opcode  : std_logic_vector(6 downto 0)
-    signal funct3  : std_logic_vector(2 downto 0)
-    signal funct7  : std_logic_vector(6 downto 0)
-    -- (ID) Signals decode
-    signal branch  : std_logic;
-    signal jump    : std_logic;
-    signal rw      : std_logic;
-    signal wb      : std_logic;
-    signal arith   : std_logic;
-    signal sign    : std_logic;
-    signal imm     : std_logic_vector(19,0);
-    signal src_imm : std_logic;
-    signal alu_op  : std_logic_vector(ALUOP_WIDTH-1 downto 0);
 
 
 begin 
 
+-- instanciate Fetch module
+-- instanciate Decode module
+-- instanciate exectue module
+-- MEMORY ACCESS
+    -- DMEM block
+    -- ME/WB Pipeline Register
+-- WRITE-BACK
+    -- Multiplexer for rd_data
 
 end architecture beh;
